03ab4c26845440e857f65988a044772aa687e096
Richard Wells <Richard.Wells@normandale.edu>
2022-01-25 10:56:45 -0600

    Documentation.

--- MediaSpaceAnalysis/split.h
+++ MediaSpaceAnalysis/split.h
@@ -4,8 +4,13 @@
 #include <string>
 #include <vector>
 
-// Split a line of data by whitespace.
+/*
+	Split a line of data by whitespace.
+*/
 std::vector<std::string> split(const std::string & s) noexcept;
 
-// Split a line of data by the given separator character.
+/*
+	Split a line of data by the given separator character.
+	Two separators in a row will be considered a blank field.
+*/
 std::vector<std::string> split(const std::string & s, char c) noexcept;
